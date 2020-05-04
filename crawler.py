4119c79fac8529f67ea97e45fdb2a243fd590e89
Krishan Patel <sc17kdp@leeds.ac.uk>
2020-05-04 13:44:39 +0100

    ranked pages

--- crawler.py
+++ crawler.py
@@ -16,7 +16,6 @@ def visible(element):
 
 def build():
     base_url = "http://example.webscraping.com"
-    # base_url = "http://example.webscraping.com/places/default/view/Canada-41"
 
     page_list = ["/"]
     dict_text = {}
@@ -31,8 +30,9 @@ def build():
         text = " ".join(t.strip() for t in visible_texts)
 
         words = text.split()
-
+        # add words to dictionary with their frequency
         for word in words:
+            word = word.replace(":","")
             if word not in dict_text:
                 dict_text[word] = {page: 1}
             else:
@@ -48,9 +48,8 @@ def build():
 
                 if link_new.get('href') == "#":
                     continue
-
                 # link_new = link_new.get("href").split("?")[0].split("index")[0].split("iso")[0].split("edit")[0]
-                link_new = link_new.get("href").split("?")[0]
+                link_new = link_new.get("href").split("?")[0].split("edit")[0]
 
                 if link_new not in page_list:
                     print(link_new)
@@ -68,38 +67,66 @@ def build():
 
 def search(keyword, data):
     # print(data[keyword])
+    print("\n")
     for url in data[keyword]:
         print(url, " ", end = "")
         print(data[keyword][url])
     return keyword
 
 def find(arguments,data):
+    print("\n")
+
     num_arguments = len(arguments)
     page_list = []
-    i = 0
     temp_list = []
-    while(i<num_arguments):
+    i = 0
+    while(i < num_arguments):
         word = arguments[i]
+        print(word)
+        # add all the pages for a particular word
         for url in data[word]:
+            # print(data[word][url])
             temp_list.append(url)
+    
         if i == 0:
             page_list = temp_list
         else:
+            # only keep the values that are in both lists
             new_list = set(page_list) & set(temp_list)
             page_list = list(new_list)
         temp_list = []
         i+=1
     # list of pages that are shared by all the arguments
-    print(page_list)
-    
+    rank(arguments, data, page_list)
+
+def rank(arguments, data, page_list):
+    counter = 0
+    rank_dict = {}
+    # gives each page a number
+    for page in page_list:
+        # initialise rank dict
+        rank_dict[page] = 0
+        for word in arguments:
+            rank_dict[page] += data[word][page]
+    # sorts the dictionary so that the highest number is first
+    sorted_dict = {k: v for k, v in sorted(rank_dict.items(), key=lambda item:item[1], reverse = True)}
+    for k,v in sorted_dict.items():
+        print(k,v)
+        
+    pass
+
 
 def main():
+    print("\n\nCommands: \n")
+    print(" build - crawls the webpage and creates an inverted index")
+    print(" load - loads the inverted index from file if one exists")
+    print(" print [word] - takes in a word and displays the inverted index for that word")
+    print(" find [query phrase] - finds a certain query phrase in the inverted index and returns a list of all pages containing this phrase ")
     while True:
-
-        user_input = input("\n\nEnter a command\n")
+        user_input = input("\n\nEnter a command: ")
 
         user_input_split = user_input.split()
-
+        # builds the index
         if user_input_split[0] == "build":
             start = timer()
             dict_text = build()
@@ -111,22 +138,22 @@ def main():
                 print("Build not complete, no file to save")
             end = timer()
             print(end - start)
-
+        # loads the index
         elif user_input_split[0] == "load":
             try:
                 with open('index.json', 'r') as fp:
                     data = json.load(fp)
-                    print(data)
+
             except:
                 print("file does not exist, run build")
-
+        # prints out
         elif user_input_split[0] == "print":
             try:
                 search(user_input_split[1], data)
             except:
                 print("error searching")
 
-
+        # finds a query phrase
         elif user_input_split[0] == "find":
             try:
                 find(user_input_split[1:], data)
